7a6df41866ccb180aa4ab7680bc73ff8a389afaf
unknown <sugamanandsahu01@gmail.com>
2022-08-14 18:06:57 +0530

    Requiring CleanWebpackPlugin for removing duplicate files on every npm run build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const config = require("./webpack.config")
 const { merge } = require("webpack-merge")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 // Merge it with webpack.config.js
 module.exports = merge (config, {
@@ -9,5 +10,6 @@ module.exports = merge (config, {
         filename: "main.[contenthash].js", // For generating a hash value for updating the cached file on browsers
         path: path.resolve(__dirname, "dist"),
         assetModuleFilename: 'images/[name].[hash][ext]' // Adding hash values to image names
-    }
+    },
+    plugins: [new CleanWebpackPlugin()] // For creating new dist or removing duplicate files fron dist
 })
