682b02066210d760d3e293945ff6db6440235c40
unknown <sugamanandsahu01@gmail.com>
2022-08-14 12:53:26 +0530

    Adding comments.

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,11 @@ const path = require("path");
 const config = require("./webpack.config")
 const { merge } = require("webpack-merge")
 
+// Merge it with webpack.config.js
 module.exports = merge (config, {
-    mode: "production",
+    mode: "production", // If it's ready to deploy
     output: {
-        filename: "main.[contenthash].js",
+        filename: "main.[contenthash].js", // For generating a hash value for updating the cached file on browsers
         path: path.resolve(__dirname, "dist")
     }
 })
