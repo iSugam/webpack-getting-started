13430032fd6e4c2bd287ac3d287f4f47ffb995e0
unknown <sugamanandsahu01@gmail.com>
2022-08-14 12:34:44 +0530

    Changing some files for dev, and prod files.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,22 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: "development",
-    devtool: false,
     entry: "./src/index.js",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
         })
     ],
+
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    "style-loader", 
+                    "css-loader"
+                ],
             },
         ]
         
