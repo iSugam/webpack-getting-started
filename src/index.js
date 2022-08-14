0e299cfaffe6091b033464f004b231621db27598
unknown <sugamanandsahu01@gmail.com>
2022-08-14 11:12:55 +0530

    Adding style-loader and css-loader to load css styles.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-document.querySelector("h1").addEventListener("click", e => e.target.style.color = "blue")
+import "./main.css"
+document.querySelector("h1").addEventListener("click", e => e.target.style.color = "#fff")
