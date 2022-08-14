d09850b4c84e460cb599310d67f26523bbeb568a
unknown <sugamanandsahu01@gmail.com>
2022-08-14 12:34:07 +0530

    Adding for production build.

---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const config = require("./webpack.config")
+const { merge } = require("webpack-merge")
+
+module.exports = merge (config, {
+    mode: "production",
+    output: {
+        filename: "main.[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    }
+})
