efbb015b8d19074788bc77afdc431f89d99e3444
unknown <sugamanandsahu01@gmail.com>
2022-08-14 12:33:50 +0530

    Adding for development.

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const path = require("path")
+const config = require("./webpack.config")
+const { merge } = require("webpack-merge")
+
+module.exports = merge(config, {
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    }
+})
