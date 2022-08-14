5a85a4eb153226d821a630e60d69fc5a7a1981e3
unknown <sugamanandsahu01@gmail.com>
2022-08-14 12:53:20 +0530

    Adding comments.

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,8 +2,9 @@ const path = require("path")
 const config = require("./webpack.config")
 const { merge } = require("webpack-merge")
 
+// Merge it with webpack.config.js
 module.exports = merge(config, {
-    mode: "development",
+    mode: "development", // If it's on development
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
